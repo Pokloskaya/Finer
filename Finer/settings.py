38c3cedb49d20fac5213881eac5c5edded37e969
Pokloskayag <master7n7@gmail.com>
2022-11-01 11:43:54 -0500

    added server IP

--- Finer/settings.py
+++ Finer/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-1c)^30g$z!s255&wo^ln5$&zpp!03*6n4fge6&a3exgh7ixxrr
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['34.27.88.129']
 
 
 # Application definition
