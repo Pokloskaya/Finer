0c8fd15b9d4c58f4f08bfce562805ca91e8062b7
Julian Romero <master7n7@gmail.com>
2022-11-10 22:56:48 -0500

    refactor: organizado el codigo de VIEWS y terminado  el indicador para saber el valor de cada minuto de trabajo

---
+++ finer/FinerApp/models.py
@@ -0,0 +1,41 @@
+from django.db import models
+from django import forms
+
+from . import choices
+
+class Empresa(models.Model):
+
+   nombre = models.CharField(max_length=50)
+   contraseña = models.CharField(max_length=20)
+   tipo_empresa = models.CharField(max_length=20,choices=choices.TIPO_EMPRESAS) #Llave foranea
+   margen_contribucion_negocio = models.DecimalField(max_digits=10,decimal_places=1,default=0)
+      
+class Producto(models.Model):
+   empresa_id = models.ForeignKey(Empresa, on_delete=models.CASCADE) #Llave foranea 
+   nombre = models.CharField(max_length=30)
+   c_v_u = models.DecimalField(max_digits=10,decimal_places=1,blank=True)  # Esto se puede calcular con base en el costo de los conceptos asociados 
+   p_v_u = models.DecimalField(max_digits=10,decimal_places=1)
+   participacion_ventas = models.DecimalField(max_digits=10,decimal_places=1)
+   margen_contribucion = models.DecimalField(max_digits=10,decimal_places=1,default=0)
+   
+
+class Concepto(models.Model):
+   producto_id = models.ForeignKey(Producto, on_delete=models.CASCADE,blank=True)
+   nombreConcepto = models.CharField(max_length=50) 
+   unidad_compra = models.CharField(max_length=30)
+   precio_compra = models.DecimalField(max_digits=10,decimal_places=1)
+   unidad_ultilizada = models.DecimalField(max_digits=10,decimal_places=1)
+   factor = models.DecimalField(max_digits=10,decimal_places=1)
+   costo_variable = models.DecimalField(max_digits=10,decimal_places=1)
+   
+class Costos(models.Model):
+   empresa_id = models.ForeignKey(Empresa, on_delete=models.CASCADE) #Llave foranea 
+   nombreCosto = models.CharField(max_length=50)
+   valorCosto = models.DecimalField(max_digits=10,decimal_places=1)
+   
+#------------------------------------------------------------------------------------
+#Cfu es para atribuirle los costos fijos al costo de un producto. Para que no sea solo con el CVU 
+class Cfu(models.Model):
+   diasLaboradosAnuales = models.DecimalField(max_digits=10,decimal_places=1)
+   jornadaDiara = models.DecimalField(max_digits=10,decimal_places=1)
+   empleadosParticipacion = models.DecimalField(max_digits=10,decimal_places=1)   
---
+++ finer/FinerApp/views.py
@@ -0,0 +1,204 @@
+from re import A
+from django.shortcuts import render, redirect,HttpResponseRedirect
+from .models import Empresa, Producto,Concepto, Costos, Cfu
+from .forms import FormEmpresa,FormCostos,FormConcepto,FormProducto, FormCfu
+
+def home(request):
+   return render(request, "home.html")
+
+#Empresa_id = 1 es una empresa productora
+#Empresa_id = 2 es una empresa comerciante
+def gestion_producto(request,empresa_id=2):
+
+   tipoEmpresa = Empresa.objects.get(id=empresa_id).tipo_empresa
+   
+   if request.method == 'POST':
+
+      form = FormProducto(request.POST)
+
+      if form.is_valid():   
+         c_v_U = form.cleaned_data['c_v_u']
+         
+      
+         if tipoEmpresa == "productor":
+            c_v_U = 0
+
+         producto = Producto.objects.create(
+         
+         empresa_id = Empresa.objects.get(id=empresa_id),
+         nombre = form.cleaned_data['nombre'],
+         c_v_u = c_v_U,
+         p_v_u = form.cleaned_data['p_v_u'],
+         participacion_ventas = form.cleaned_data['participacion_ventas'])
+
+         producto.save()
+
+
+   productos = Producto.objects.filter(empresa_id=empresa_id)
+   form = FormProducto()
+
+   return render(request, "gestion_producto.html", {"productos": productos,'form':form,'tipoEmpresa':Empresa.objects.get(id=empresa_id).tipo_empresa})
+
+#----------------------------------------------------------------------------------------------------
+
+def obtenerDiasLaborados():
+       
+   consultaDiasTrabajados = Cfu.objects.values_list('diasLaboradosAnuales', flat=True)
+   consultaHorasLaborados = Cfu.objects.values_list('jornadaDiara', flat=True)
+
+   diasTrabajados = sum(consultaDiasTrabajados)
+   horasLaboradas = sum(consultaHorasLaborados)
+
+   diasTrabajadosMes = diasTrabajados/12
+   horasTrabajoTotal = diasTrabajadosMes * horasLaboradas
+   egresosFijosMes = obtenerCostoTotalEmpresa()
+   valorPorMinuto = (egresosFijosMes / horasTrabajoTotal) / 60
+
+
+   return valorPorMinuto
+
+
+def indicadores(request):
+
+   if request.method == 'POST':
+    
+      form = FormCfu(request.POST)
+
+      if form.is_valid():
+         form.save()
+         return redirect("/indicadores")
+      else:
+         print(form.errors)
+         
+
+   form = FormCfu()
+   diasTrabajados = obtenerDiasLaborados()
+
+   return render(request, "indicadores.html", {'prueba':diasTrabajados, 'form':form})
+
+#----------------------------------------------------------------------------------------------------------------------
+
+#----------------------PRODUCTO----------------------------------------
+def eliminar_producto(request, producto_id):
+   producto = Producto.objects.get(empresa_id = 1,id = producto_id) 
+   producto.delete()
+   
+   return redirect("/productos")
+   
+def eliminar_costo(request, costo_id):
+   
+   costo = Costos.objects.get(empresa_id = 1,id = costo_id) 
+   costo.delete()
+   
+   return redirect("/costosfijos")
+
+def editar_producto(request,producto_id,empresa_id=1):
+    
+   tipoEmpresa = Empresa.objects.get(id=empresa_id).tipo_empresa
+   producto = Producto.objects.get(id = producto_id)
+   data = {"producto":producto,'tipoEmpresa':tipoEmpresa}
+   
+   if request.method == 'GET':
+      
+      if tipoEmpresa == 'productor':
+         
+         conceptos = Concepto.objects.filter(producto_id=producto_id)
+         formConcepto = FormConcepto()            
+         data['conceptos'] = conceptos
+         data['formConcepto'] = formConcepto
+      
+      
+   elif request.method == 'POST':
+    
+      form = FormProducto(request.POST)
+      if form.has_changed() and form.is_valid():
+         
+         if tipoEmpresa == 'productor':
+            c_v_u = 0
+         elif tipoEmpresa == 'comerciante':
+            c_v_u = form.cleaned_data['c_v_u']
+          
+         producto.nombre = form.cleaned_data['nombre']
+         producto.c_v_u = c_v_u
+         producto.p_v_u = form.cleaned_data['p_v_u']
+         producto.participacion_ventas = form.cleaned_data['participacion_ventas']
+         producto.save()
+         
+      return redirect(f"/productos/editar/{producto_id}")
+         
+   data['form'] = FormProducto(instance=producto)
+
+   return render(request, "editar_producto.html",data)
+#------------------------------------------------------------------------------
+
+def añadir_concepto(request, producto_id):
+   if request.method == 'POST':
+      form = FormConcepto(request.POST)
+
+      if form.is_valid():
+         concepto = form.save(commit=False)
+         concepto.producto_id = Producto.objects.get(id = producto_id)
+         concepto.save()
+      else:
+         print(form.errors)
+
+   return redirect(f'/productos/editar/{producto_id}')
+
+def eliminar_concepto(request,concepto_id):
+       
+   concepto = Concepto.objects.get(id=concepto_id)
+   productoid = concepto.producto_id.empresa_id
+   concepto.delete()
+   
+   return redirect(f"/productos/editar/{productoid}")
+
+def registro(request):
+       data = {'form': FormEmpresa()}
+       if request.method == 'POST':
+              formulario = FormEmpresa(request.POST)
+              if formulario.is_valid():
+                  formulario.save()
+                         
+       return render(request, "registro.html",data)
+
+
+def costos_fijos(request,empresa_id=1):
+   if request.method == 'POST':
+            
+      costo = FormCostos(request.POST)
+      if costo.is_valid():
+         costo = costo.save(commit=False)
+         costo.empresa_id = Empresa.objects.get(id=empresa_id)      
+         costo.save()
+        
+   costos = Costos.objects.filter(empresa_id=empresa_id)
+   sumCostos = obtenerCostoTotalEmpresa()
+   formCostos = FormCostos()
+   
+   return render(request,"costosfijos.html",{'costos':costos,'formCostos':formCostos,'costosTotales':sumCostos})
+
+def obtenerCostoTotalEmpresa():
+   amounts = Costos.objects.values_list('valorCosto', flat=True)
+   total = sum(amounts)
+   
+   return total
+
+
+
+
+
+   
+   
+   
+
+
+         
+
+
+
+
+         
+
+
+
+   
